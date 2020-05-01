2143d8487c42452018e02b0542c314a456381091
laudisio <rafalau2202@gmail.com>
2020-04-30 21:48:41 -0300

    Versão 1.0

---
+++ App.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import { StyleSheet, TextInput, View, Button, FlatList, Keyboard, Text } from 'react-native';
+import CidadeItem from './Components/CidadeItem';
+
+export default function App() {
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/weather?lang=pt&units=metric&q=";
+  const apiKey = '2b792622ce8c3d07e59dbb0eb97735e2';
+
+  const [cidade, setCidade] = useState('');
+  const [previsoes, setPrevisoes] = useState([]);
+  const [error, setError] = useState(false)
+
+  const capturarCidade = (cidade) => {
+    setCidade(cidade);
+  }
+
+  const obtemPrevisoes = () => {
+    setPrevisoes([]);
+
+  const target = endPoint + cidade + "&appid=" + apiKey;
+    fetch(target)
+    .then((dados) => dados.json())
+    .then((dados) => {
+      if(dados.cod && dados.cod == 200 ){
+        setError(false);
+      } else  {
+        setError(true);
+      }
+      setPrevisoes([dados])
+      Keyboard.dismiss()
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.entrada}>
+      <TextInput
+      style={styles.nomeCidade}
+      placeholder="Digite o nome de uma cidade"
+      value={cidade}
+      onChangeText={capturarCidade}
+      />
+      <Button title="Ok" onPress={obtemPrevisoes}/>
+      </View>
+      {error?
+        <Text style={styles.errorMessage}>Cidade não encontrada</Text>
+        :
+        <FlatList
+          data={previsoes}
+          renderItem={
+            previsao => (
+            <CidadeItem previsao={previsao} />
+          )
+          }
+        />
+
+      }
+    </View>
+    );
+    
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  nomeCidade: {
+    padding: 10,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    flexGrow: 0.9
+  },
+  entrada: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8
+  },
+  errorMessage: {
+    color: '#FF0000'
+  }
+});
